bafa74e27a708b59c174b67a3e0c482d91cd93ab
destradax <destradax@gmail.com>
2014-11-28 09:58:02 -0500

    Added Author resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.delete_all
+authors = ["Martin Fowler",
+  "James Martin",
+  "Terry Quatrani",
+  "Dave Marcum",
+  "Stuart McClure",
+  "Jakob Nielsen",
+  "Mark C. Chan",
+  "Rick Decker",
+  "Walter B. Meigs",
+  "Ivar Jacobson",
+  "Angela Hatton",
+  "Brian Clegg",
+  "Antonio Díaz Morales",
+  "Guillermo D'Andrea",
+  "Lawrence G. Friedman",
+  "Paul Hyde",
+  "Alexis Leon",
+  "Carlos Scheel Mayenberger",
+  "Derek Escorsa Lastells",
+  "María del Pilar Roa Paez",
+  "Mary Beth Chrissis",
+  "Michael Morrison",
+  "Craig Larman",
+  "Len Bass",
+  "Vicente Ambrósio",
+  "Dirk Krafzig",
+  "Diana Neira",
+  "Christopher Alberts",
+  "Walter Fumy",
+  "Dean Leffingwell",
+  "Dr. Fadil Pedic",
+  "John Leppard"]
+
+authors.each do |author|
+  Author.create(name: author)
+end
